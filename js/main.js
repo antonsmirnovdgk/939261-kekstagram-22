be32cee22137e6b81ccc41fe92f8ce45fd01f9f3
Anton Smirnov <>
2021-01-31 17:44:18 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const str = document.querySelector('.social__footer-text');
+const str = 'asdsafdfdfdfgfdgh';
 
 const getRandomNum =  function(minNumber, maxNumber) {
   const randomNum = Math.floor(Math.random() * maxNumber); // Math.random() выведете рандомно число от 0 до 1 , потом мы умножим на maxNubmer - это будет число  с запятой , а floor его округлит к меньшему значению
@@ -8,9 +8,14 @@ const getRandomNum =  function(minNumber, maxNumber) {
 window.console.log(getRandomNum(1, 10));
 
 
-const checkLengthOfStr = (str, maxLength = 140) => str.length <= maxLength;
+// const checkLengthOfStr = (str, maxLength = 140) => Number(str.length) <= maxLength;
 
-window.console.log(checkLengthOfStr.length(str));
+// window.console.log(checkLengthOfStr(str));
 
 
 
+const checkLengthOfStr = function(str, maxLength = 140) {
+  return Number(str.length) <= maxLength;
+}
+
+window.console.log(checkLengthOfStr(str));
