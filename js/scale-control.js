6ddc4a769618cd83b0185a331f372529adbae22a
Anton Smirnov <>
2021-02-28 13:20:50 +0300

    6 Задание

--- js/scale-control.js
+++ js/scale-control.js
@@ -1,20 +1,20 @@
 const valueField = document.querySelector('.scale__control--value');
-const biggerButton = document.querySelector('.scale__control--bigger');
-// const bigImg = document.querySelector('.img-upload__preview');
-const smallerButton = document.querySelector('.scale__control--smaller');
+const onBiggerButton = document.querySelector('.scale__control--bigger');
+const bigImg = document.querySelector('.img-upload__preview');
+const onSmallerButton = document.querySelector('.scale__control--smaller');
 
 valueField.value = 100 + '%';
 
-biggerButton.addEventListener('click', function(evt){
-  evt.preventDefault();
+onBiggerButton.addEventListener('click', function(){
   if(parseInt(valueField.value) < 100){
     valueField.value = parseInt(valueField.value) + 25 + '%';
+    bigImg.style.transform = bigImg.style.transform + 'scale' - '(0.25)';
   }
 })
 
-smallerButton.addEventListener('click', function(evt){
-  evt.preventDefault();
+onSmallerButton.addEventListener('click', function(){
   if(parseInt(valueField.value) > 0){
     valueField.value = parseInt(valueField.value) - 25 + '%';
+    bigImg.style.transform = bigImg.style.transform + 'scale' + '(0.25)';
   }
 })
