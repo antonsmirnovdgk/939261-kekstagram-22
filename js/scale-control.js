44552a7e5f79d327a74ea2608d10258357e2bbc1
Anton Smirnov <>
2021-03-01 23:13:32 +0300

    Сделаны фильтры для изображения

--- js/scale-control.js
+++ js/scale-control.js
@@ -2,81 +2,82 @@ const valueField = document.querySelector('.scale__control--value');
 const onBiggerButton = document.querySelector('.scale__control--bigger');
 const bigImg = document.querySelector('.img-upload__preview');
 const onSmallerButton = document.querySelector('.scale__control--smaller');
-const onRadioEffect = document.querySelectorAll('.effects__radio');
+// const onRadioEffect = document.querySelectorAll('.effects__radio');
 const effectField = document.querySelector('.effect-level__slider');
 const effectValue = document.querySelector('.effect-level__value');
-let a = 25;
+const formImgUpload = document.querySelector('.img-upload');
+let scaleStep = 100;
 
 
 valueField.value = 100 + '%';
 
 onBiggerButton.addEventListener('click', function(){
   if(parseInt(valueField.value) < 100){
-    a = a + 25;
-    valueField.value = a + '%';
-    bigImg.style.transform = `scale(${a / 100})`;
+    scaleStep = scaleStep + 25;
+    valueField.value = scaleStep + '%';
+    bigImg.style.transform = `scale(${scaleStep / 100})`;
   }
 })
 
 onSmallerButton.addEventListener('click', function(){
   if(parseInt(valueField.value) > 25){
-    a = a - 25;
-    window.console.log(a);
-    valueField.value = a + '%';
-    bigImg.style.transform = `scale(${a / 100})`;
+    scaleStep = scaleStep - 25;
+    valueField.value = scaleStep + '%';
+    bigImg.style.transform = `scale(${scaleStep / 100})`;
   }
 })
 
 
 
+// eslint-disable-next-line no-undef
 noUiSlider.create(effectField, {
   range: {
     min: 0,
     max: 1,
   },
-  start: 0,
+  start: 1,
   step: 0.1,
 });
 
 
-effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-  effectValue.value = unencoded[handle];
-  // bigImg.style.filter = 'grayscale('+effectValue.value+')';
-});
+document.querySelector('.noUi-target').classList.add('visually-hidden')
 
+const onParrentUploadContaierCLick = function(evt){
 
-const updateSlider = function(effect){
-  effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-    effectValue.value = unencoded[handle];
-    bigImg.style.filter = effect;
-  })
-}
+  if(evt.target.matches('input[type="radio"]')){
 
-
-onRadioEffect.forEach((item) => {
-  item.addEventListener('change', (evt) => {
 
     if(evt.target.checked && evt.target.id === 'effect-none') {
 
-      updateSlider('none');
+      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+        effectValue.value = unencoded[handle];
+        bigImg.style.filter = 'none';
+      });
+
+
+      document.querySelector('.noUi-target').classList.add('visually-hidden')
     }
 
     if(evt.target.checked && evt.target.id === 'effect-chrome') {
-      bigImg.classList.add('effects__preview--chrome')
+      bigImg.classList.add('effects__preview--chrome');
+
 
       effectField.noUiSlider.updateOptions({
         range: {
           min: 0,
           max: 1,
         },
+
         step: 0.1,
+        start: 1,
       });
 
       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
         effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'grayscale(' + effectValue.value + ')';
+        bigImg.style.filter = 'grayscale('+effectValue.value+')';
       });
 
+      document.querySelector('.noUi-target').classList.remove('visually-hidden');
     }
 
     if(evt.target.checked && evt.target.id === 'effect-sepia') {
@@ -89,14 +90,15 @@ onRadioEffect.forEach((item) => {
         },
         step: 0.1,
       });
+
+
       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
         effectValue.value = unencoded[handle];
         bigImg.style.filter = 'sepia('+effectValue.value+')';
       });
 
+      document.querySelector('.noUi-target').classList.remove('visually-hidden');
     }
-
-
 
     if(evt.target.checked && evt.target.id === 'effect-marvin') {
       bigImg.classList.add('effects__preview--marvin')
@@ -106,13 +108,15 @@ onRadioEffect.forEach((item) => {
           min: 0,
           max: 100,
         },
-        step: 1,
-      });
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'invert('+effectValue.value+ '%)';
+        start: 100,
       });
 
+      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+        effectValue.value = unencoded[handle];
+        bigImg.style.filter = 'invert(' + effectValue.value + '%)';
+      });
+
+      document.querySelector('.noUi-target').classList.remove('visually-hidden');
     }
 
 
@@ -125,12 +129,17 @@ onRadioEffect.forEach((item) => {
           min: 0,
           max: 3,
         },
+        start: 3,
         step: 0.1,
       });
+
+
       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
         effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'blur('+effectValue.value+')';
+        bigImg.style.filter = 'blur(' + effectValue.value + 'px)';
       });
+
+      document.querySelector('.noUi-target').classList.remove('visually-hidden');
     }
 
 
@@ -143,15 +152,138 @@ onRadioEffect.forEach((item) => {
           min: 1,
           max: 3,
         },
+        start: 3,
         step: 0.1,
       });
+
+
       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
         effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'brightness('+effectValue.value+')';
+        bigImg.style.filter = 'brightness(' + effectValue.value + ')';
       });
-    }
 
-  });
-});
+      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+    }
+  }
+}
+
+formImgUpload.addEventListener('change', onParrentUploadContaierCLick)
+
+
+// onRadioEffect.forEach((item) => {
+//   item.addEventListener('change', (evt) => {
+
+//     document.querySelector('.noUi-target').classList.add('visually-hidden');
+
+//     if(evt.target.checked && evt.target.id === 'effect-none') {
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'none';
+//       })
+
+//     }
+
+//     if(evt.target.checked && evt.target.id === 'effect-chrome') {
+//       bigImg.classList.add('effects__preview--chrome')
+
+//       effectField.noUiSlider.updateOptions({
+//         range: {
+//           min: 0,
+//           max: 1,
+//         },
+
+//         step: 0.1,
+//         start: 1,
+//       });
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'grayscale('+effectValue.value+')';
+//       })
+
+//       document.querySelector('.noUi-target').classList.remove('visually-hidden');
+//     }
+
+//     if(evt.target.checked && evt.target.id === 'effect-sepia') {
+//       bigImg.classList.add('effects__preview--sepia')
+
+//       effectField.noUiSlider.updateOptions({
+//         range: {
+//           min: 0,
+//           max: 1,
+//         },
+//         step: 0.1,
+//       });
+
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'sepia('+effectValue.value+')';
+//       })
+
+//     }
+
+//     if(evt.target.checked && evt.target.id === 'effect-marvin') {
+//       bigImg.classList.add('effects__preview--marvin')
+
+//       effectField.noUiSlider.updateOptions({
+//         range: {
+//           min: 0,
+//           max: 100,
+//         },
+//         start: 100,
+//       });
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'invert(' + effectValue.value + '%)';
+//       })
+//     }
+
+
+
+//     if(evt.target.checked && evt.target.id === 'effect-phobos') {
+//       bigImg.classList.add('effects__preview--phobos')
+
+//       effectField.noUiSlider.updateOptions({
+//         range: {
+//           min: 0,
+//           max: 3,
+//         },
+//         start: 3,
+//         step: 0.1,
+//       });
+
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'blur(' + effectValue.value + 'px)';
+//       })
+//     }
+
+
+
+//     if(evt.target.checked && evt.target.id === 'effect-heat') {
+//       bigImg.classList.add('effects__preview--heat')
+
+//       effectField.noUiSlider.updateOptions({
+//         range: {
+//           min: 1,
+//           max: 3,
+//         },
+//         start: 3,
+//         step: 0.1,
+//       });
+
+
+//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+//         effectValue.value = unencoded[handle];
+//         bigImg.style.filter = 'brightness(' + effectValue.value + ')';
+//       })
+//     }
+
+//   });
+// });
 
 
