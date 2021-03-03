3c7a2fd3c3a10c47c198a06e007d3ec21099aaca
Anton Smirnov <>
2021-03-03 18:15:25 +0300

    Устранены недочеты 6го задания части 1

--- js/scale-control.js
+++ js/scale-control.js
@@ -2,34 +2,51 @@ const valueField = document.querySelector('.scale__control--value');
 const onBiggerButton = document.querySelector('.scale__control--bigger');
 const bigImg = document.querySelector('.img-upload__preview');
 const onSmallerButton = document.querySelector('.scale__control--smaller');
-// const onRadioEffect = document.querySelectorAll('.effects__radio');
 const effectField = document.querySelector('.effect-level__slider');
 const effectValue = document.querySelector('.effect-level__value');
 const formImgUpload = document.querySelector('.img-upload');
-let scaleStep = 100;
+const uploadOverlay = formImgUpload.querySelector('.img-upload__overlay');
+const onUploadButton = formImgUpload.querySelector('.img-upload__label');
+const onCancelButton = formImgUpload.querySelector('.img-upload__cancel');
+const sliderLevel = document.querySelector('.effect-level');
+let scaleStep = 25;
+const STEP = 25;
+const SCALE_BUTTOM = 25;
+const SCALE_TOP = 100;
 
 
-valueField.value = 100 + '%';
+
+onUploadButton.addEventListener('click', function() {
+  uploadOverlay.classList.remove('hidden')
+});
+
+
+onCancelButton.addEventListener('click', function(){
+  uploadOverlay.classList.add('hidden');
+  bigImg.style.filter = 'none';
+  sliderLevel.classList.add('visually-hidden');
+})
+
+valueField.value = `${SCALE_TOP}%`;
 
 onBiggerButton.addEventListener('click', function(){
-  if(parseInt(valueField.value) < 100){
-    scaleStep = scaleStep + 25;
-    valueField.value = scaleStep + '%';
-    bigImg.style.transform = `scale(${scaleStep / 100})`;
+  if(parseInt(valueField.value) < SCALE_TOP){
+    scaleStep = scaleStep + STEP;
+    valueField.value = `${scaleStep}%`;
+    bigImg.style.transform = 'scale' + '(' + scaleStep / 100 + ')';
   }
 })
 
 onSmallerButton.addEventListener('click', function(){
-  if(parseInt(valueField.value) > 25){
-    scaleStep = scaleStep - 25;
-    valueField.value = scaleStep + '%';
-    bigImg.style.transform = `scale(${scaleStep / 100})`;
+  if(parseInt(valueField.value) > SCALE_BUTTOM){
+    scaleStep = parseInt(valueField.value) - STEP;
+    valueField.value = `${scaleStep}%`;
+    bigImg.style.transform = 'scale' + '(' + scaleStep / 100 + ')';
   }
 })
 
+/* global noUiSlider:readonly */
 
-
-// eslint-disable-next-line no-undef
 noUiSlider.create(effectField, {
   range: {
     min: 0,
@@ -37,10 +54,30 @@ noUiSlider.create(effectField, {
   },
   start: 1,
   step: 0.1,
+  connect: 'lower',
 });
 
 
-document.querySelector('.noUi-target').classList.add('visually-hidden')
+const setEffectFieldValue = function(filterValue, value)  {
+  effectField.noUiSlider.on('update', (_, handle, unencoded) => {
+    effectValue.value = unencoded[handle];
+    bigImg.style.filter = `${filterValue}` + '(' + effectValue.value + `${value}`; ')';
+  });
+}
+
+const updateOptions = function(min, max, start, step) {
+  effectField.noUiSlider.updateOptions({
+    range: {
+      min,
+      max,
+    },
+    step,
+    start,
+  });
+}
+
+
+document.querySelector('.effect-level').classList.add('visually-hidden')
 
 const onParrentUploadContaierCLick = function(evt){
 
@@ -49,74 +86,41 @@ const onParrentUploadContaierCLick = function(evt){
 
     if(evt.target.checked && evt.target.id === 'effect-none') {
 
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'none';
-      });
+      sliderLevel.classList.add('visually-hidden');
 
-
-      document.querySelector('.noUi-target').classList.add('visually-hidden')
+      bigImg.style.filter = 'none';
     }
+
 
     if(evt.target.checked && evt.target.id === 'effect-chrome') {
       bigImg.classList.add('effects__preview--chrome');
 
+      updateOptions(0, 1, 1, 0.1)
 
-      effectField.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
+      setEffectFieldValue('grayscale' , '');
 
-        step: 0.1,
-        start: 1,
-      });
-
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'grayscale('+effectValue.value+')';
-      });
-
-      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+      sliderLevel.classList.remove('visually-hidden')
     }
+
 
     if(evt.target.checked && evt.target.id === 'effect-sepia') {
       bigImg.classList.add('effects__preview--sepia')
 
-      effectField.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
-        step: 0.1,
-      });
+      updateOptions(0, 1, 1, 0.1)
 
+      setEffectFieldValue('sepia' , '');
 
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'sepia('+effectValue.value+')';
-      });
-
-      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+      sliderLevel.classList.remove('visually-hidden')
     }
 
     if(evt.target.checked && evt.target.id === 'effect-marvin') {
       bigImg.classList.add('effects__preview--marvin')
 
-      effectField.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 100,
-        },
-        start: 100,
-      });
+      updateOptions(0, 100, 100, 1);
 
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'invert(' + effectValue.value + '%)';
-      });
+      setEffectFieldValue('invert' , '%');
 
-      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+      sliderLevel.classList.remove('visually-hidden')
     }
 
 
@@ -124,22 +128,11 @@ const onParrentUploadContaierCLick = function(evt){
     if(evt.target.checked && evt.target.id === 'effect-phobos') {
       bigImg.classList.add('effects__preview--phobos')
 
-      effectField.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
+      updateOptions(0, 3, 3, 0.1);
 
+      setEffectFieldValue('blur', 'px');
 
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'blur(' + effectValue.value + 'px)';
-      });
-
-      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+      sliderLevel.classList.remove('visually-hidden')
     }
 
 
@@ -147,143 +140,16 @@ const onParrentUploadContaierCLick = function(evt){
     if(evt.target.checked && evt.target.id === 'effect-heat') {
       bigImg.classList.add('effects__preview--heat')
 
-      effectField.noUiSlider.updateOptions({
-        range: {
-          min: 1,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
+      updateOptions(1, 3, 3, 0.1);
 
+      setEffectFieldValue('brightness' , '');
 
-      effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-        effectValue.value = unencoded[handle];
-        bigImg.style.filter = 'brightness(' + effectValue.value + ')';
-      });
-
-      document.querySelector('.noUi-target').classList.remove('visually-hidden');
+      sliderLevel.classList.remove('visually-hidden')
     }
+
   }
 }
 
 formImgUpload.addEventListener('change', onParrentUploadContaierCLick)
-
-
-// onRadioEffect.forEach((item) => {
-//   item.addEventListener('change', (evt) => {
-
-//     document.querySelector('.noUi-target').classList.add('visually-hidden');
-
-//     if(evt.target.checked && evt.target.id === 'effect-none') {
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'none';
-//       })
-
-//     }
-
-//     if(evt.target.checked && evt.target.id === 'effect-chrome') {
-//       bigImg.classList.add('effects__preview--chrome')
-
-//       effectField.noUiSlider.updateOptions({
-//         range: {
-//           min: 0,
-//           max: 1,
-//         },
-
-//         step: 0.1,
-//         start: 1,
-//       });
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'grayscale('+effectValue.value+')';
-//       })
-
-//       document.querySelector('.noUi-target').classList.remove('visually-hidden');
-//     }
-
-//     if(evt.target.checked && evt.target.id === 'effect-sepia') {
-//       bigImg.classList.add('effects__preview--sepia')
-
-//       effectField.noUiSlider.updateOptions({
-//         range: {
-//           min: 0,
-//           max: 1,
-//         },
-//         step: 0.1,
-//       });
-
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'sepia('+effectValue.value+')';
-//       })
-
-//     }
-
-//     if(evt.target.checked && evt.target.id === 'effect-marvin') {
-//       bigImg.classList.add('effects__preview--marvin')
-
-//       effectField.noUiSlider.updateOptions({
-//         range: {
-//           min: 0,
-//           max: 100,
-//         },
-//         start: 100,
-//       });
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'invert(' + effectValue.value + '%)';
-//       })
-//     }
-
-
-
-//     if(evt.target.checked && evt.target.id === 'effect-phobos') {
-//       bigImg.classList.add('effects__preview--phobos')
-
-//       effectField.noUiSlider.updateOptions({
-//         range: {
-//           min: 0,
-//           max: 3,
-//         },
-//         start: 3,
-//         step: 0.1,
-//       });
-
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'blur(' + effectValue.value + 'px)';
-//       })
-//     }
-
-
-
-//     if(evt.target.checked && evt.target.id === 'effect-heat') {
-//       bigImg.classList.add('effects__preview--heat')
-
-//       effectField.noUiSlider.updateOptions({
-//         range: {
-//           min: 1,
-//           max: 3,
-//         },
-//         start: 3,
-//         step: 0.1,
-//       });
-
-
-//       effectField.noUiSlider.on('update', (_, handle, unencoded) => {
-//         effectValue.value = unencoded[handle];
-//         bigImg.style.filter = 'brightness(' + effectValue.value + ')';
-//       })
-//     }
-
-//   });
-// });
 
 
