022938fa10aa96ca805995ff75265353a3ec9e32
Anton Smirnov <>
2021-03-06 21:57:24 +0300

    merge

--- js/upload-file.js
+++ js/upload-file.js
@@ -3,34 +3,25 @@ import {isEscEvent} from './utils.js';
 import {onFieldFocus} from './valid-hashtags.js';
 
 
-
-
 const uploadField = document.querySelector('#upload-file');
 const imgUpload = document.querySelector('.img-upload__overlay');
 const closeUploadButton = document.querySelector('#upload-cancel');
 const onFocusText = document.querySelector('.img-upload__text');
 
 
-
 // Открытие поп-ап
 uploadField.addEventListener('change', function(){
   resetSlider();
   document.addEventListener('keydown', onEscKeyDown);
-<<<<<<< HEAD
   onFocusText.addEventListener('focusin', onFieldFocus);
-=======
   document.body.classList.add('modal-open');
   imgUpload.classList.remove('hidden');
->>>>>>> module6-task1
 });
-
-
 
 
 //закрытие поп-ап
 closeUploadButton.addEventListener('click', function(){
   document.removeEventListener('keydown', onEscKeyDown);
-  onFocusText.removeEventListener('focusin', onFieldFocus);
   closeImg();
 });
 
