ddad8f277e3c938f738ff8c2499b110d360c4890
Anton Smirnov <>
2021-03-14 22:28:55 +0300

    .

--- js/img-filters.js
+++ js/img-filters.js
@@ -1,20 +1,21 @@
 import {getRandomNum} from './utils.js';
 
 const imgFilterElement = document.querySelector('.img-filters');
-const imgFilterFormElement = document.querySelector('.img-filters__form');
+const filterRandomElement = document.querySelector('#filter-random');
+
+// const imgFilterFormElement = document.querySelector('.img-filters__form');
 
 
-const onFilterFormClick = function(evt){
-  if(evt.target.id === 'filter-random'){
-    evt.target.classList.add('img-filters__button--active')
+// const onFilterFormClick = function(evt){
+//   if(evt.target.id === 'filter-random'){
+//     evt.target.classList.add('img-filters__button--active')
 
-  }else {
-    evt.target.classList.remove('img-filters__button--active')
-  }
-}
+//   }else {
+//     evt.target.classList.remove('img-filters__button--active')
+//   }
+// }
 
-
-imgFilterFormElement.addEventListener('click', onFilterFormClick);
+// imgFilterFormElement.addEventListener('click', onFilterFormClick);
 
 
 
@@ -24,24 +25,25 @@ const showImgFilterElement = function(){
 }
 
 
-// let arrONe = ['qwe', 'ewq', 'qwe', 'asd', 'asd', 'aaa', 'ccc', 'ddd', 'aaa', 'cdc', 'ascd', 'asd', 'bbb', 'qwe', 'ewq', 'qwe', 'asd', 'asd', 'aaa', 'ccc', 'ddd', 'aaa', 'cdc', 'ascd', 'asd', 'bbb'];
+let arrONe = ['qwe', 'ewq', 'qwe', 'asd', 'asd', 'aaa', 'ccc', 'ddd', 'aaa', 'cdc', 'ascd', 'asd', 'bbb', 'qwe', 'ewq', 'qwe', 'asd', 'asd', 'aaa', 'ccc', 'ddd', 'aaa', 'cdc', 'ascd', 'asd', 'bbb'];
+
 
 const filterRandom = function(array){
 
-  const result = [];
 
-  for(let i = 0; result.length < 10; i++) {
+  // const filterRandom = function(array){
+//   const result = [];
+//   for(let i = 0; result.length < 10; i++) {
+//     const randomElement = array[getRandomNum(0, array.length - 1)];
+//     if (!result.includes(randomElement)) {
+//       result.push(randomElement);
+//     }
+//   }
+//   return result;
+// }
 
-    const randomElement = array[getRandomNum(0, array.length - 1)];
 
-    if (!result.includes(randomElement)) {
-      result.push(randomElement);
-    }
-  }
-  return result;
-}
+filterRandomElement.addEventListener('click', filterRandom(arrONe));
 
-
-// filterRandomElement.addEventListener('click', filterRandom(arrONe));
 
 export {showImgFilterElement}
